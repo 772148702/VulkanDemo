9f05d52a4401d491ed49048d625ef90a953cc8e3
lzy <772148702@qq.com>
2022-08-29 21:26:56 +0800

    ex6_init

---
+++ src/Example/6_ImageGUI/xmake.lua
@@ -0,0 +1,7 @@
+target("6_ImageGUI")
+set_kind("binary")
+add_files("/*.cpp","../LaunchWindows.cpp")
+add_links(links_list)
+add_includedirs(include_dir_list, "$(projectdir)/src/Engine")
+add_ldflags("-subsystem:windows")
+add_deps("Vulkan")
