353610de024d0d973c9f67c7c801f590575a883e
lzy <772148702@qq.com>
2022-09-01 00:05:42 +0800

    ex12_update

--- src/Example/12_PushConstants/12_PushConstants.cpp
+++ src/Example/12_PushConstants/12_PushConstants.cpp
@@ -21,16 +21,16 @@
 #include "imgui.h"
 #include "vulkan/vulkan_core.h"
 
-class TextureModule : public DemoBase
+class PushConstantsModule : public DemoBase
 {
 public:
-    TextureModule(int32 width, int32 height, const char* title, const std::vector<std::string>& cmdLine)
+    PushConstantsModule(int32 width, int32 height, const char* title, const std::vector<std::string>& cmdLine)
         : DemoBase(width, height, title, cmdLine)
     {
 
     }
 
-    virtual ~TextureModule()
+    virtual ~PushConstantsModule()
     {
 
     }
@@ -83,9 +83,21 @@ public:
 
 private:
 
-   struct MVPBlock
+//    struct MVPBlock
+//     {
+//         Matrix4x4 model;
+//         Matrix4x4 view;
+//         Matrix4x4 projection;
+//     };
+
+
+    struct ModelPushConstantBlock
     {
         Matrix4x4 model;
+    };
+
+    struct ViewProjectionBlock
+    {
         Matrix4x4 view;
         Matrix4x4 projection;
     };
@@ -107,6 +119,8 @@ private:
     {
 
         int32 bufferIndex = DemoBase::AcquireBackbufferIndex();
+
+  
         bool hovered = UpdateUI(time, delta);
         if (!hovered)
         {
@@ -125,25 +139,12 @@ private:
         {
             ImGui::SetNextWindowPos(ImVec2(0, 0));
             ImGui::SetNextWindowSize(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);
-            ImGui::Begin("Texture", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-            ImGui::Text("SSS Demo");
-
-            for (int32 i = 0; i < m_Model->meshes.size(); ++i)
-            {
-                vk_demo::DVKMesh* mesh = m_Model->meshes[i];
-                ImGui::Text("%-20s Tri:%d", mesh->linkNode->name.c_str(), mesh->triangleCount);
-            }
-
-            ImGui::SliderFloat("Curvature", &(m_ParamData.curvature),       0.0f, 10.0f);
-            ImGui::SliderFloat2("CurvatureBias", (float*)&(m_ParamData.curvatureScaleBias), 0.0f, 1.0f);
-
-            ImGui::SliderFloat("BlurredLevel", &(m_ParamData.blurredLevel), 0.0f, 12.0f);
-            ImGui::SliderFloat("Exposure", &(m_ParamData.exposure),         0.0f, 10.0f);
-
-            ImGui::SliderFloat3("LightDirection", (float*)&(m_ParamData.lightDir), -10.0f, 10.0f);
-            ImGui::ColorEdit3("LightColor", (float*)&(m_ParamData.lightColor));
+            ImGui::Begin("PushConstants", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+            ImGui::Text("Renderabls");
+            ImGui::Checkbox("AutoRotate", &m_AutoRotate);
 
             ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+ 
             ImGui::End();
         }
 
@@ -153,7 +154,7 @@ private:
 
         if (m_GUI->Update())
         {
-            SetupCommandBuffers();
+           // SetupCommandBuffers();
         }
 
         return hovered;
@@ -208,6 +209,15 @@ private:
 
             for (int32 meshIndex = 0; meshIndex < m_Model->meshes.size(); ++meshIndex)
             {
+                const Matrix4x4& globalMatrix = m_Model->meshes[meshIndex]->linkNode->GetGlobalMatrix();
+                vkCmdPushConstants(
+                m_CommandBuffers[i],
+                m_PipelineLayout,
+                VK_SHADER_STAGE_VERTEX_BIT,
+                0,
+                sizeof(ModelPushConstantBlock),
+                &globalMatrix
+                );
                 m_Model->meshes[meshIndex]->BindDrawCmd(m_CommandBuffers[i]);
             }
 
@@ -220,15 +230,14 @@ private:
 
     void CreateDescriptorSet()
     {
-        VkDescriptorPoolSize poolSizes[2];
+        VkDescriptorPoolSize poolSizes[1];
         poolSizes[0].type            = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        poolSizes[0].descriptorCount = 2;
-        poolSizes[1].type            = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        poolSizes[1].descriptorCount = 4;
+        poolSizes[0].descriptorCount = 1;
+
 
         VkDescriptorPoolCreateInfo descriptorPoolInfo;
         ZeroVulkanStruct(descriptorPoolInfo, VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO);
-        descriptorPoolInfo.poolSizeCount = 2;
+        descriptorPoolInfo.poolSizeCount = 1;
         descriptorPoolInfo.pPoolSizes    = poolSizes;
         descriptorPoolInfo.maxSets       = 1;
         VERIFYVULKANRESULT(vkCreateDescriptorPool(m_Device, &descriptorPoolInfo, VULKAN_CPU_ALLOCATOR, &m_DescriptorPool));
@@ -246,30 +255,11 @@ private:
         writeDescriptorSet.dstSet          = m_DescriptorSet;
         writeDescriptorSet.descriptorCount = 1;
         writeDescriptorSet.descriptorType  = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        writeDescriptorSet.pBufferInfo     = &(m_MVPBuffer->descriptor);
+        writeDescriptorSet.pBufferInfo     = &(m_ViewProjBuffer->descriptor);
         writeDescriptorSet.dstBinding      = 0;
         vkUpdateDescriptorSets(m_Device, 1, &writeDescriptorSet, 0, nullptr);
 
-        ZeroVulkanStruct(writeDescriptorSet, VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
-        writeDescriptorSet.dstSet          = m_DescriptorSet;
-        writeDescriptorSet.descriptorCount = 1;
-        writeDescriptorSet.descriptorType  = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        writeDescriptorSet.pBufferInfo     = &(m_ParamBuffer->descriptor);
-        writeDescriptorSet.dstBinding      = 1;
-        vkUpdateDescriptorSets(m_Device, 1, &writeDescriptorSet, 0, nullptr);
-
-        std::vector<vk_demo::DVKTexture*> textures = { m_TexDiffuse, m_TexNormal, m_TexCurvature, m_TexPreIntegrated };
-        for (int32 i = 0; i < 4; ++i)
-        {
-            ZeroVulkanStruct(writeDescriptorSet, VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
-            writeDescriptorSet.dstSet          = m_DescriptorSet;
-            writeDescriptorSet.descriptorCount = 1;
-            writeDescriptorSet.descriptorType  = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-            writeDescriptorSet.pBufferInfo     = nullptr;
-            writeDescriptorSet.pImageInfo      = &(textures[i]->descriptorInfo);
-            writeDescriptorSet.dstBinding      = 2 + i;
-            vkUpdateDescriptorSets(m_Device, 1, &writeDescriptorSet, 0, nullptr);
-        }
+    
     }
 
     void CreateDescriptorPool()
@@ -295,8 +285,8 @@ private:
         std::vector<VkVertexInputAttributeDescription> vertexInputAttributs = m_Model->GetInputAttributes();
 
         vk_demo::DVKGfxPipelineInfo pipelineInfo;
-        pipelineInfo.vertShaderModule = vk_demo::LoadSPIPVShader(m_Device, "assets/shaders/11_Texture/texture.vert.spv");
-        pipelineInfo.fragShaderModule = vk_demo::LoadSPIPVShader(m_Device, "assets/shaders/11_Texture/texture.frag.spv");
+        pipelineInfo.vertShaderModule = vk_demo::LoadSPIPVShader(m_Device, "assets/shaders/12_PushConstants/obj.vert.spv");
+        pipelineInfo.fragShaderModule = vk_demo::LoadSPIPVShader(m_Device, "assets/shaders/12_PushConstants/obj.frag.spv");
         m_Pipelines[0] = vk_demo::DVKGfxPipeline::Create(m_VulkanDevice, m_PipelineCache, pipelineInfo, { vertexInputBinding }, vertexInputAttributs, m_PipelineLayout, m_RenderPass);
 
         vkDestroyShaderModule(m_Device, pipelineInfo.vertShaderModule, VULKAN_CPU_ALLOCATOR);
@@ -314,38 +304,32 @@ private:
 
     void CreateDescriptorSetLayout()
     {
-        VkDescriptorSetLayoutBinding layoutBindings[6] = { };
+        VkDescriptorSetLayoutBinding layoutBindings[1] = { };
         layoutBindings[0].binding            = 0;
         layoutBindings[0].descriptorType     = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
         layoutBindings[0].descriptorCount    = 1;
         layoutBindings[0].stageFlags         = VK_SHADER_STAGE_VERTEX_BIT;
         layoutBindings[0].pImmutableSamplers = nullptr;
 
-        layoutBindings[1].binding            = 1;
-        layoutBindings[1].descriptorType     = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        layoutBindings[1].descriptorCount    = 1;
-        layoutBindings[1].stageFlags         = VK_SHADER_STAGE_FRAGMENT_BIT;
-        layoutBindings[1].pImmutableSamplers = nullptr;
-
-        for (int32 i = 0; i < 4; ++i)
-        {
-            layoutBindings[2 + i].binding            = 2 + i;
-            layoutBindings[2 + i].descriptorType     = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-            layoutBindings[2 + i].descriptorCount    = 1;
-            layoutBindings[2 + i].stageFlags         = VK_SHADER_STAGE_FRAGMENT_BIT;
-            layoutBindings[2 + i].pImmutableSamplers = nullptr;
-        }
 
         VkDescriptorSetLayoutCreateInfo descSetLayoutInfo;
         ZeroVulkanStruct(descSetLayoutInfo, VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO);
-        descSetLayoutInfo.bindingCount = 6;
+        descSetLayoutInfo.bindingCount = 1;
         descSetLayoutInfo.pBindings    = layoutBindings;
         VERIFYVULKANRESULT(vkCreateDescriptorSetLayout(m_Device, &descSetLayoutInfo, VULKAN_CPU_ALLOCATOR, &m_DescriptorSetLayout));
+
+
+        VkPushConstantRange pushConstantRange = {};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+        pushConstantRange.offset     = 0;
+        pushConstantRange.size       = sizeof(ModelPushConstantBlock);
 
         VkPipelineLayoutCreateInfo pipeLayoutInfo;
         ZeroVulkanStruct(pipeLayoutInfo, VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO);
         pipeLayoutInfo.setLayoutCount = 1;
         pipeLayoutInfo.pSetLayouts    = &m_DescriptorSetLayout;
+        pipeLayoutInfo.pushConstantRangeCount = 1;
+        pipeLayoutInfo.pPushConstantRanges    = &pushConstantRange;
         VERIFYVULKANRESULT(vkCreatePipelineLayout(m_Device, &pipeLayoutInfo, VULKAN_CPU_ALLOCATOR, &m_PipelineLayout));
     }
 
@@ -360,14 +344,17 @@ private:
     {
         if (m_AutoRotate)
         {
-            m_MVPData.model.AppendRotation(90.0f * delta, Vector3::UpVector);
+            m_Model->rootNode->localMatrix.AppendRotation(30.0f * delta, Vector3::UpVector);
+           // m_MVPData.model.AppendRotation(90.0f * delta, Vector3::UpVector);
         }
 
-        m_MVPData.view = m_ViewCamera.GetView();
-        m_MVPData.projection = m_ViewCamera.GetProjection();
+        m_ViewProjData.view = m_ViewCamera.GetView();
+        m_ViewProjData.projection = m_ViewCamera.GetProjection();
 
-        m_MVPBuffer->CopyFrom(&m_MVPData, sizeof(MVPBlock));
-        m_ParamBuffer->CopyFrom(&m_ParamData, sizeof(ParamBlock));
+        m_ViewProjBuffer->CopyFrom(&m_ViewProjData, sizeof(ViewProjectionBlock));
+        // m_ParamBuffer->CopyFrom(&m_ParamData, sizeof(ParamBlock));
+        //why we need to do here
+        SetupCommandBuffers();
     }
 
     void CreateUniformBuffers()
@@ -376,49 +363,47 @@ private:
         Vector3 boundSize   = bounds.max - bounds.min;
         Vector3 boundCenter = bounds.min + boundSize * 0.5f;
 
-        m_MVPData.model.AppendRotation(180, Vector3::UpVector);
-
-        m_ViewCamera.Perspective(PI / 4, GetWidth() / 3.0f, GetHeight(), 0.1f, 1000.0f);
-        m_ViewCamera.SetPosition(boundCenter.x, boundCenter.y, boundCenter.z - boundSize.Size() * 2.0f);
+        m_ViewCamera.Perspective(PI / 4, GetWidth(), GetHeight(), 100.0f, 5000.0f);
+        m_ViewCamera.SetPosition(boundCenter.x, boundCenter.y + 1000, boundCenter.z - boundSize.Size() * 1.5f);
         m_ViewCamera.LookAt(boundCenter);
 
-        m_MVPBuffer = vk_demo::DVKBuffer::CreateBuffer(
+        m_ViewProjBuffer = vk_demo::DVKBuffer::CreateBuffer(
             m_VulkanDevice,
             VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            sizeof(MVPBlock),
-            &(m_MVPData)
+            sizeof(m_ViewProjData),
+            &(m_ViewProjData)
         );
-        m_MVPBuffer->Map();
+        m_ViewProjBuffer->Map();
 
         
-        m_ParamData.blurredLevel = 2.0;
-        m_ParamData.curvature = 3.5;
-        m_ParamData.curvatureScaleBias.x = 0.101;
-        m_ParamData.curvatureScaleBias.y = -0.001;
-        m_ParamData.exposure = 1.0;
-        m_ParamData.lightColor.Set(240.0f / 255.0f, 200.0f / 255.0f, 166.0f / 255.0f);
-        m_ParamData.lightDir.Set(1, 0, -1.0);
-        m_ParamData.lightDir.Normalize();
-        m_ParamData.padding = 0.0;
+        // m_ParamData.blurredLevel = 2.0;
+        // m_ParamData.curvature = 3.5;
+        // m_ParamData.curvatureScaleBias.x = 0.101;
+        // m_ParamData.curvatureScaleBias.y = -0.001;
+        // m_ParamData.exposure = 1.0;
+        // m_ParamData.lightColor.Set(240.0f / 255.0f, 200.0f / 255.0f, 166.0f / 255.0f);
+        // m_ParamData.lightDir.Set(1, 0, -1.0);
+        // m_ParamData.lightDir.Normalize();
+        // m_ParamData.padding = 0.0;
 
-        m_ParamBuffer = vk_demo::DVKBuffer::CreateBuffer(
-            m_VulkanDevice,
-            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            sizeof(ParamBlock),
-            &(m_ParamData)
-        );
-        m_ParamBuffer->Map();
+        // m_ParamBuffer = vk_demo::DVKBuffer::CreateBuffer(
+        //     m_VulkanDevice,
+        //     VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        //     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        //     sizeof(ParamBlock),
+        //     &(m_ParamData)
+        // );
+        // m_ParamBuffer->Map();
     }
 
     void DestroyUniformBuffers()
     {
-         m_MVPBuffer->UnMap();
-        delete m_MVPBuffer;
+         m_ViewProjBuffer->UnMap();
+        delete m_ViewProjBuffer;
 
-        m_ParamBuffer->UnMap();
-        delete m_ParamBuffer;
+        // m_ParamBuffer->UnMap();
+        // delete m_ParamBuffer;
     }
 
 
@@ -439,19 +424,12 @@ private:
     {
         vk_demo::DVKCommandBuffer* cmdBuffer = vk_demo::DVKCommandBuffer::Create(m_VulkanDevice,m_CommandPool);
         
-        m_Model = vk_demo::DVKModel::LoadFromFile(
-            "assets/models/head.obj",
+          m_Model = vk_demo::DVKModel::LoadFromFile(
+            "assets/models/Room/miniHouse_FBX.FBX",
             m_VulkanDevice,
             cmdBuffer,
-            { VertexAttribute::VA_Position, VertexAttribute::VA_UV0, VertexAttribute::VA_Normal, VertexAttribute::VA_Tangent }
+            { VertexAttribute::VA_Position, VertexAttribute::VA_UV0, VertexAttribute::VA_Normal }
         );
-
-
-
-        m_TexDiffuse       = vk_demo::DVKTexture::Create2D("assets/textures/head_diffuse.jpg", m_VulkanDevice, cmdBuffer);
-        m_TexNormal        = vk_demo::DVKTexture::Create2D("assets/textures/head_normal.jpg", m_VulkanDevice, cmdBuffer);
-        m_TexCurvature     = vk_demo::DVKTexture::Create2D("assets/textures/curvatureLUT.png", m_VulkanDevice, cmdBuffer);
-        m_TexPreIntegrated = vk_demo::DVKTexture::Create2D("assets/textures/preIntegratedLUT.png", m_VulkanDevice, cmdBuffer);            
         
         delete cmdBuffer;
     }
@@ -459,11 +437,6 @@ private:
     void DestroyAssets()
     {
         delete m_Model;
-
-        delete m_TexDiffuse;
-        delete m_TexNormal;
-        delete m_TexCurvature;
-        delete m_TexPreIntegrated;
     }
 
 private:
@@ -475,11 +448,12 @@ private:
 
     vk_demo::DVKCamera              m_ViewCamera;
 
-    MVPBlock                        m_MVPData;
-    vk_demo::DVKBuffer*             m_MVPBuffer;
+   
+    ViewProjectionBlock             m_ViewProjData;
+    vk_demo::DVKBuffer*             m_ViewProjBuffer = nullptr;
 
-    ParamBlock                      m_ParamData;
-    vk_demo::DVKBuffer*             m_ParamBuffer = nullptr;
+    // ParamBlock                      m_ParamData;
+    // vk_demo::DVKBuffer*             m_ParamBuffer = nullptr;
 
     vk_demo::DVKTexture*            m_TexDiffuse = nullptr;
     vk_demo::DVKTexture*            m_TexNormal = nullptr;
@@ -500,5 +474,5 @@ private:
 
 std::shared_ptr<AppModuleBase> CreateAppMode(const std::vector<std::string>& cmdLine)
 {
-    return std::make_shared<TextureModule>(1400, 900, "Texture", cmdLine);
+    return std::make_shared<PushConstantsModule>(1400, 900, "PushConstantsModule", cmdLine);
 }
