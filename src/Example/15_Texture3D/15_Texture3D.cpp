503890a73106815432c185f5cba2e896cd81d8db
zhyuli <zhyuli@tencent.com>
2022-09-01 17:43:01 +0800

    ex15_update

--- src/Example/15_Texture3D/15_Texture3D.cpp
+++ src/Example/15_Texture3D/15_Texture3D.cpp
@@ -404,35 +404,30 @@ private:
 
     void CreateDescriptorSetLayout()
     {
-        std::vector<VkDescriptorSetLayoutBinding> layoutBindings(4);
+         std::vector<VkDescriptorSetLayoutBinding> layoutBindings(4);
         layoutBindings[0].binding            = 0;
         layoutBindings[0].descriptorType     = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
         layoutBindings[0].descriptorCount    = 1;
         layoutBindings[0].stageFlags         = VK_SHADER_STAGE_VERTEX_BIT;
         layoutBindings[0].pImmutableSamplers = nullptr;
 
-
         layoutBindings[1].binding            = 1;
-        layoutBindings[1].descriptorType     = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC;
+        layoutBindings[1].descriptorType     = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
         layoutBindings[1].descriptorCount    = 1;
-        layoutBindings[1].stageFlags         = VK_SHADER_STAGE_VERTEX_BIT;
+        layoutBindings[1].stageFlags         = VK_SHADER_STAGE_FRAGMENT_BIT;
         layoutBindings[1].pImmutableSamplers = nullptr;
 
-
-        //COMBINED_IMAGE_SAMPLER, what is this meaning
         layoutBindings[2].binding            = 2;
         layoutBindings[2].descriptorType     = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
         layoutBindings[2].descriptorCount    = 1;
         layoutBindings[2].stageFlags         = VK_SHADER_STAGE_FRAGMENT_BIT;
         layoutBindings[2].pImmutableSamplers = nullptr;
 
-        
         layoutBindings[3].binding            = 3;
         layoutBindings[3].descriptorType     = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
         layoutBindings[3].descriptorCount    = 1;
         layoutBindings[3].stageFlags         = VK_SHADER_STAGE_FRAGMENT_BIT;
         layoutBindings[3].pImmutableSamplers = nullptr;
-
 
         VkDescriptorSetLayoutCreateInfo descSetLayoutInfo;
         ZeroVulkanStruct(descSetLayoutInfo, VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO);
