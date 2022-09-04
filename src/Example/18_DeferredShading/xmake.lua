9eeabe2da3d50b17fa7615eb02f344256cbecce1
lzy <772148702@qq.com>
2022-09-04 22:29:49 +0800

    ex18_init

---
+++ src/Example/18_DeferredShading/xmake.lua
@@ -0,0 +1,7 @@
+target("18_DeferredShading")
+set_kind("binary")
+add_files("/*.cpp","../LaunchWindows.cpp")
+add_links(links_list)
+add_includedirs(include_dir_list, "$(projectdir)/src/Engine")
+add_ldflags("-subsystem:windows")
+add_deps("Vulkan")
